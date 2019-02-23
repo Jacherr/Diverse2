671d01ceb9d6e91b6c75e8afc214ddf1cca17d3d
Jacher <jwc2002@outlook.com>
2019-02-23 11:34:52 +0000

    sdffds

--- commands/Searching/discriminator.js
+++ commands/Searching/discriminator.js
@@ -19,7 +19,7 @@ module.exports = {
         }
         msg.channel.createMessage({
             embed: {
-                description: b.join(',\n'),
+                description: b.join('\n'),
                 color: 0x22BB55
             }
         });
