b5ff50d6a56afc8bf110d807d305a206eaac730b
Jacher <jwc2002@outlook.com>
2019-03-08 17:53:43 +0000

    sad

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -9,7 +9,7 @@ module.exports = {
         let query = args.join(" ")
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
-            else if(rows != undefined) return msg.channel.createMessage(rows[0].toString())
+            else if(rows != undefined) return msg.channel.createMessage(rows[0].inspect(rows[0], { depth : 0 }))
             else return
         })
     },
