c7c7b3ff1f7827ad5801e0c33859d9e4f5712060
Jacher <jwc2002@outlook.com>
2019-03-09 23:01:49 +0000

    dscfdsfc

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -8,11 +8,11 @@ module.exports = {
     generator: async (msg, args) => {
         if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
         else if(args[0] == 'xp') con = mysql.createConnection(dbdetails.mysqlxp)
-        else return msg.channel.createMessage('not valid lol')
+        else return msg.channel.createMessage('not valid')
         let query = args.join(" ")
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
-            else if(rows != undefined) return msg.channel.createMessage(rows.toString())
+            else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
             else return
         })
     },
