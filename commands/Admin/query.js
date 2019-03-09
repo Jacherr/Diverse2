067c818809bc3789dd961528d011b7201683eeba
Jacher <jwc2002@outlook.com>
2019-03-09 23:00:47 +0000

    bcbvc

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -1,16 +1,18 @@
 const dbdetails = require('../../config.json')
-let mysql = require('mysql')
+const mysql = require('mysql')
 let con
 module.exports = {
     label: 'query',
     enabled: true,
     isSubcommand: false,
     generator: async (msg, args) => {
-        con = mysql.createConnection(dbdetails.mysqltest)
+        if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
+        else if(args[0] == 'xp') con = mysql.createConnection(dbdetails.mysqlxp)
+        else return msg.channel.createMessage('not valid lol')
         let query = args.join(" ")
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
-            else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
+            else if(rows != undefined) return msg.channel.createMessage(rows.toString())
             else return
         })
     },
