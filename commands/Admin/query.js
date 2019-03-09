1fa8823b83027f02c4fd14152c7eacfd32fff80a
Jacher <jwc2002@outlook.com>
2019-03-09 22:58:42 +0000

    vc

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -6,10 +6,8 @@ module.exports = {
     enabled: true,
     isSubcommand: false,
     generator: async (msg, args) => {
-        if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
-        else if(args[0] == 'xp') con = mysql.createConnection(dbdetails.mysqlxp)
-        else return msg.channel.createMessage('not a valid db my broski')
-        let query = args.join(" ").shift()
+        con = mysql.createConnection(dbdetails.mysqltest)
+        let query = args.join(" ")
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
             else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
