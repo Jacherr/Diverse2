33e63b117ad59c975684e4046fad45605b59e84f
Jacher <jwc2002@outlook.com>
2019-03-09 23:11:27 +0000

    d

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -9,7 +9,7 @@ module.exports = {
         if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
         else if(args[0] == 'xp') con = mysql.createConnection(dbdetails.mysqlxp)
         else return msg.channel.createMessage('not valid')
-        let query = args.join(" ")
+        let query = args.join(" ").shift()
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
             else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
