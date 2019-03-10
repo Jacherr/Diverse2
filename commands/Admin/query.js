dd9f5ff64a8e913435858a2280eb357504f59504
Jacher <jwc2002@outlook.com>
2019-03-10 17:31:10 +0000

    vbvb

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -10,6 +10,9 @@ module.exports = {
         else if(args[0] == 'xpdb') con = mysql.createConnection(dbdetails.mysqlxp)
         else return msg.channel.createMessage('not valid')
         console.log(args)
+        args.shift()
+        console.log(args)
+        let query = args.join(" ")
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
             else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
