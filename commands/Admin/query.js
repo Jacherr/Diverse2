9576773d90414ed04a85ba4d3c01d7b2a0eb06c5
Jacher <jwc2002@outlook.com>
2019-03-10 17:26:05 +0000

    fgdfgd

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -9,7 +9,8 @@ module.exports = {
         if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
         else if(args[0] == 'xpdb') con = mysql.createConnection(dbdetails.mysqlxp)
         else return msg.channel.createMessage('not valid')
-        query = args.join(" ").slice(5, args.join(" ").length - 1)
+        query = args
+        query.shift()
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
             else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
