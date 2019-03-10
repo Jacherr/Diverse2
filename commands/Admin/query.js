34114e779b7add8a47a93af99fa15cf8ba851821
Jacher <jwc2002@outlook.com>
2019-03-10 17:24:29 +0000

    fdggf

--- commands/Admin/query.js
+++ commands/Admin/query.js
@@ -7,10 +7,9 @@ module.exports = {
     isSubcommand: false,
     generator: async (msg, args) => {
         if(args[0] == 'test') con = mysql.createConnection(dbdetails.mysqltest)
-        else if(args[0] == 'xp') con = mysql.createConnection(dbdetails.mysqlxp)
+        else if(args[0] == 'xpdb') con = mysql.createConnection(dbdetails.mysqlxp)
         else return msg.channel.createMessage('not valid')
-        let query = args.splice(0, 1)
-        console.log(query)
+        query = args.join(" ").slice(5, args.join(" ").length - 1)
         con.query(query, (err, rows) => {
             if(err) return msg.channel.createMessage(err.message) 
             else if(rows != undefined) return msg.channel.createMessage(require('util').inspect(rows))
