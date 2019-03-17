66623300ee12d08377ffc469aacc5bb2e63cda7e
Jacher <jwc2002@outlook.com>
2019-03-17 18:21:51 +0000

    TESTING

--- commands/Misc/rextester2.js
+++ commands/Misc/rextester2.js
@@ -4,7 +4,7 @@ const languageProperties = [
         importType: 'using',
         lineBreak = ';',
         defaultImports: ['System'],
-        aliases: ['1'],
+        aliases: ['1', 'c#'],
         classDeclaration: `namespace Rextester
         {
             public class Program
@@ -21,7 +21,7 @@ const languageProperties = [
         importType: 'import',
         lineBreak: ';',
         defaultImports: ['java.lang.*'],
-        aliases: ['4'],
+        aliases: ['4', 'java'],
         classDeclaration: `class Rextester
         {
             public static void main(String args[])
@@ -35,7 +35,7 @@ const languageProperties = [
         importType: 'Imports',
         lineBreak: undefined,
         defaultImports: ['System', 'System.Collections.Generic', 'System.Linq', 'System.Text.RegularExpressions'],
-        aliases: ['visualbasic', '2'],
+        aliases: ['visualbasic', '2', 'vb'],
         classDeclaration: `Namespace Rextester
             Public Module Program
                 Public Sub Main(args() As string)
@@ -49,7 +49,7 @@ const languageProperties = [
         importType: 'import',
         lineBreak: undefined,
         defaultImports: ['math', 'random'],
-        aliases: ['python', 'python3', 'py3', 'python2.7', 'py2.7', 'py2', 'python2', '5', '24'],
+        aliases: ['python', 'python3', 'py3', 'python2.7', 'py2.7', 'py2', 'python2', '5', '24', 'py'],
         classDeclaration: undefined
     }
 ]
@@ -59,7 +59,46 @@ module.exports = {
     enabled: true,
     isSubcommand: false,
     generator: async (msg, args) => {
-
+        let language = args[0];
+        let parseLangs = languageProperties.filter(i => i.classDeclaration != undefined).name;
+        let languageObject;
+        let imports = [];
+        languageProperties.forEach(curobject => {
+            if(curobject.aliases.includes(language)) {
+                languageObject = curobject
+            }
+        });
+        if(parseLangs.includes(languageObject.name) && args[1] == languageObject.importType) {
+            for(let i = 2; i < args.length; i++){  
+                if(args[i].endsWith(';')) {
+                    imports.push(args[i].substr(0, args[i].length - 1));
+                    i = args.length + 1;
+                } else {
+                    imports.push(args[i]);
+                }
+            };
+        };
+        if(args[1] == languageObject.importType && parseLangs.includes(languageObject.name)) {
+            args.splice(0, imports.length + 2)
+        } else {
+            args.splice(0, 1)
+        }
+        let code = args.join(" ")
+        if(languageObject.classDeclaration != undefined) {
+            for(let i = 0; i < languageObject.classDeclaration.length - 8; i++) {
+                let currentPrase = languageObject.classDeclaration[i] + languageObject.classDeclaration[i + 1] + languageObject.classDeclaration[i + 2] + languageObject.classDeclaration[i + 3] + languageObject.classDeclaration[i + 4] + languageObject.classDeclaration[i + 5] + languageObject.classDeclaration[i + 6] + languageObject.classDeclaration[i + 7]
+                if(currentPrase == '{{code}}') {
+                    let currentDec = languageObject.classDeclaration
+                    let control
+                    let control2
+                    control = currentDec.slice(0, i - 1)
+                    control2 = currentDec.slice(i + 8, currentDec.length)
+                    currentDec = control + "\n" + code + "\n" + control2
+                }
+            }
+        }
+        console.log(code)
+        console.log(language)
     },
     options: {
         description: 'This is a testing command',
