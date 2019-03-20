093ad928477025cd930d603f0c93f1289025d152
Jacher <jwc2002@outlook.com>
2019-03-20 21:12:42 +0000

    test

--- commands/Misc/rextester2.js
+++ commands/Misc/rextester2.js
@@ -85,8 +85,39 @@ function parseLanguages(args, imports) {
     };
 }
 
-const config = require('../../config.json')
 const superagent = require('superagent')
+
+function outputResult(msg, language, code) {
+    superagent
+    .post('https://fapi.wrmsr.io/rextester')
+    .set({
+        Authorization: config.api,
+        "Content-Type": "application/json"
+    })
+    .send({
+        args: {
+            text: code,
+            language: language
+        }
+    })
+    .end((err, response) => {
+        if (err) {
+            message.edit(`${err.toString()}`);
+        }
+        else {
+            message.delete();
+            if(response.text.length == 0) return msg.channel.createMessage("Empty response")
+            if (response.text.length > 1900) {
+                let responsetext = response.text.substr(0, 1900)
+                msg.channel.createMessage(`\`\`\`${responsetext}\`\`\``)
+            } else {
+                msg.channel.createMessage(`\`\`\`${response.text}\`\`\``)
+            }
+        };
+    });
+}
+
+const config = require('../../config.json')
 
 module.exports = {
     label: 'rextester2',
