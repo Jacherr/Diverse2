813e856cad25e3a1d4e9459eee8bafa30dcee697
Jacher <jwc2002@outlook.com>
2019-03-20 21:29:44 +0000

    UPDATED AND MERGED COMMAND

--- commands/Misc/rextester.js
+++ commands/Misc/rextester.js
@@ -1,171 +1,96 @@
-const config = require('../../config.json')
-const utils = require('../../utils/utils.js')
-const superagent = require('superagent')
-module.exports = {
-    label: 'rextester',
-    enabled: true,
-    isSubcommand: false,
-    generator: async (msg, args) => {
-        let imports = []
-        let language
-        if(args[0] != undefined) language = args[0].toLowerCase()  
-        let importLangs = ['vb', 'visualbasic', '2', 'c#', '1', 'java', '4']
-        if(args[0] == undefined) return outputFullHelp()
-        if(args[0] == 'list') return msg.channel.createMessage("https://hb.wrmsr.io/siraqazugo")
-        if(args[0] == 'imports') return outputDefaultImports()
-        let message = await msg.channel.createMessage("Processing, please wait...")
-        function outputDefaultImports() {
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: msg.member.username,
-                        icon_url: msg.member.avatarURL
-                    },
-                    thumbnail: {
-                        url: `https://rextester.com/Content/linqdb_logo.png`
-                    },
-                    color: 0x44ff77,
-                    title: `**Rextester default imports**`,
-                    fields: [
-                        {
-                            name: `C#`,
-                            value: `\`System\``,
-                            inline: true
-                        },
-                        {
-                            name: `Python`,
-                            value: `\`math\`, \`random\``,
-                            inline: true
-                        },
-                        {
-                            name: `Java`,
-                            value: `\`java.lang.*\``,
-                            inline: true
-                        }
-                    ],
-                    footer: {
-                        text: `Duplicate imports are ignored for languages that can parse imports.`
-                    }
+const languageProperties = [
+    {
+        name: 'c#',
+        importType: 'using',
+        lineBreak: ';',
+        defaultImports: ['System'],
+        aliases: ['1', 'c#'],
+        classDeclaration: `namespace Rextester
+        {
+            public class Program
+            {
+                public static void Main(string[] args)
+                {
+                    {{code}}
                 }
-            })
+            }
+        }`
+    },
+    {
+        name: 'java',
+        importType: 'import',
+        lineBreak: ';',
+        defaultImports: ['java.lang.*'],
+        aliases: ['4', 'java'],
+        classDeclaration: `class Rextester
+        {
+            public static void main(String args[])
+            {
+                {{code}}
+            }
+        }`
+    },
+    {
+        name: 'vb',
+        importType: 'Imports',
+        lineBreak: '',
+        defaultImports: ['System', 'System.Collections.Generic', 'System.Linq', 'System.Text.RegularExpressions'],
+        aliases: ['visualbasic', '2', 'vb'],
+        classDeclaration: `Namespace Rextester
+            Public Module Program
+                Public Sub Main(args() As string)
+                    {{code}}
+                End Sub
+            End Module
+        End Namespace`
+    },
+    {
+        name: 'py',
+        importType: 'import',
+        lineBreak: '',
+        defaultImports: ['math', 'random'],
+        aliases: ['python', 'python3', 'py3', 'python2.7', 'py2.7', 'py2', 'python2', '5', '24', 'py'],
+        classDeclaration: undefined
+    }
+]
+
+function parseCode(code, languageObject, imports) {
+    if (languageObject.classDeclaration != undefined) {
+        let classDec = languageObject.classDeclaration
+        classDec = classDec.replace("{{code}}", code)
+        code = classDec
+    }
+    let i = 0
+    imports.forEach(element => {
+        if (languageObject.defaultImports.includes(element)) {
+            imports.splice(i, 1)
         }
-        function outputFullHelp() {
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: msg.member.username,
-                        icon_url: msg.member.avatarURL
-                    },
-                    thumbnail: {
-                        url: `https://rextester.com/Content/linqdb_logo.png`
-                    },
-                    color: 0x44ff77,
-                    title: `**Rextester**`,
-                    description: `This command allows you to write and compile code in a variety of languages.`,
-                    fields: [
-                        {
-                            name: `Usage`,
-                            value: `The general usage for all languages is \`..rextester [language] [code]\`, but some languages support imports as well as code and language.\nIn this case, you can use \`..rextester [language] [import <imports>]; [code] and your imports will be parsed for you at the top of the code.`,
-                            inline: false
-                        },
-                        {
-                            name: `Examples`,
-                            value: `**For most languages:**\n \`..rextester node console.log('Hello World!')\`\n\`..rextester py print('Hello World')\`\n\n**For C#, Visual Basic and Java:**\n\`..rextester c# import System; Console.WriteLine("Hello World!");\`\n\`..rextester java import java.lang.*; System.out.println("Hello World!");\`\n*Imports are not mandatory for these languages - in other words, you can completely disregard the \`import\` statement if you want and the code will still run.*`,
-                            imline: false
-                        },
-                        {
-                            name: `Extra Information`,
-                            value: `- Certain languages have default imports. To see them, try \`..rextester imports\`\n- Don't include a linebreak before your first line of code, or it will break\n- You can get a full list of supported languages by Rextester with \`..rex list\``,
-                            inline: false
-                        }
-                    ]
-                }
-            })
-        }
-        function parseImports(importType, postImport) {
-            imports.forEach(element => {
-                code = `${importType} ${element}${postImport}\n${code}`
-            });
-        }
-        function fetchImports() {
-            for(let i = 2; i < args.length; i++){  
-                if(args[i].endsWith(';')) {
-                    imports.push(args[i].substr(0, args[i].length - 1))
-                    i = args.length + 1
-                } else {
-                    imports.push(args[i])
-                }
-            };
-        }
-        function removeDuplicates(defaultImports) {
-            let i = 0
-            imports.forEach(element => {
-                if(defaultImports.includes(element)) {
-                    imports.splice(i, 1)
-                }
-                i++
-            });
-            defaultImports.forEach(element => {
-                imports.push(element)
-            })
-        }
-        function genCSharp() {
-            code = `namespace Rextester\n
-                    {\n
-                        public class Program\n
-                        {\n
-                            public static void Main(string[] args)\n
-                            {\n
-                                ${code}
-                            }\n
-                        }\n
-                    }`
-            removeDuplicates(['System'])
-            parseImports('using', ';')
-        }
-        function genJava() {
-            code = `class Rextester\n
-            {\n
-                public static void main(String args[])\n
-                {\n
-                    ${code}
-                }\n
-            }`
-            removeDuplicates(['java.lang.*'])
-            parseImports('import', ';')
-        }
-        function genVisualBasic() {
-            code = `         
-            Namespace Rextester\n
-                Public Module Program\n
-                    Public Sub Main(args() As string)\n
-                        ${code}
-                    End Sub\n
-                End Module\n
-            End Namespace`
-            parseImports('Imports', '')
-        }
-        function genPython() {
-            imports = ['math', 'random']
-            parseImports('import', '')
-        }
-        if(args[1] == 'import' && importLangs.includes(language)) {
-            fetchImports()
-            args.splice(0, imports.length + 2)
+        i++
+    });
+    languageObject.defaultImports.forEach(element => {
+        imports.push(element)
+    })
+    imports.forEach(element => {
+        code = `${languageObject.importType} ${element}${languageObject.lineBreak}\n${code}`
+    });
+    return code
+}
+
+function parseLanguages(args, imports) {
+    for (let i = 2; i < args.length; i++) {
+        if (args[i].endsWith(';')) {
+            imports.push(args[i].substr(0, args[i].length - 1));
+            i = args.length + 1;
         } else {
-            args.splice(0, 1)
+            imports.push(args[i]);
         }
-        let code = args.join(" ")
-        if(language == 'c#' || language == '1') {
-            genCSharp()
-        } else if(language == 'java' || language == '4') {
-            genJava()
-        } else if(language == 'vb' || language == 'visualbasic' || language == '2') {
-            genVisualBasic()
-        } else if(language == 'py' || language == 'py3' || language == 'python' || language == 'python3' || language == '24' || language == 'python2' || language == 'python2.7' || language == 'py2.7' || language == 'py2') {
-            genPython()
-        }
-        let value = await superagent
+    };
+}
+
+const superagent = require('superagent')
+
+function outputResult(msg, language, code, message) {
+    superagent
         .post('https://fapi.wrmsr.io/rextester')
         .set({
             Authorization: config.api,
@@ -183,7 +108,7 @@ module.exports = {
             }
             else {
                 message.delete();
-                if(response.text.length == 0) return msg.channel.createMessage("Empty response")
+                if (response.text.length == 0) return msg.channel.createMessage("Empty response")
                 if (response.text.length > 1900) {
                     let responsetext = response.text.substr(0, 1900)
                     msg.channel.createMessage(`\`\`\`${responsetext}\`\`\``)
@@ -192,11 +117,131 @@ module.exports = {
                 }
             };
         });
+}
+
+function outputFullHelp(msg) {
+    msg.channel.createMessage({
+        embed: {
+            author: {
+                name: msg.member.username,
+                icon_url: msg.member.avatarURL
+            },
+            thumbnail: {
+                url: `https://rextester.com/Content/linqdb_logo.png`
+            },
+            color: 0x44ff77,
+            title: `**Rextester**`,
+            description: `This command allows you to write and compile code in a variety of languages.`,
+            fields: [
+                {
+                    name: `Usage`,
+                    value: `The general usage for all languages is \`..rextester [language] [code]\`, but some languages support imports as well as code and language.\nIn this case, you can use \`..rextester [language] [import <imports>]; [code] and your imports will be parsed for you at the top of the code.`,
+                    inline: false
+                },
+                {
+                    name: `Examples`,
+                    value: `**For most languages:**\n \`..rextester node console.log('Hello World!')\`\n\`..rextester py print('Hello World')\`\n\n**For C#, Visual Basic and Java:**\n\`..rextester c# import System; Console.WriteLine("Hello World!");\`\n\`..rextester java import java.lang.*; System.out.println("Hello World!");\`\n*Imports are not mandatory for these languages - in other words, you can completely disregard the \`import\` statement if you want and the code will still run.*`,
+                    imline: false
+                },
+                {
+                    name: `Extra Information`,
+                    value: `- Certain languages have default imports. To see them, try \`..rextester imports\`\n- Don't include a linebreak before your first line of code, or it will break\n- You can get a full list of supported languages by Rextester with \`..rex list\``,
+                    inline: false
+                }
+            ]
+        }
+    })
+}
+
+function outputImports(msg) {
+    msg.channel.createMessage({
+        embed: {
+            author: {
+                name: msg.member.username,
+                icon_url: msg.member.avatarURL
+            },
+            thumbnail: {
+                url: `https://rextester.com/Content/linqdb_logo.png`
+            },
+            color: 0x44ff77,
+            title: `**Rextester default imports**`,
+            fields: [
+                {
+                    name: `C#`,
+                    value: `\`System\``,
+                    inline: true
+                },
+                {
+                    name: `Python`,
+                    value: `\`math\`, \`random\``,
+                    inline: true
+                },
+                {
+                    name: `Java`,
+                    value: `\`java.lang.*\``,
+                    inline: true
+                },
+                {
+                    name: `Visual Basic`,
+                    value: `\`System\`, \`System.Collections.Generic\`, \`System.Linq\`, \`System.Text.RegularExpressions\``,
+                    inline: true
+                }
+            ],
+            footer: {
+                text: `Duplicate imports are ignored (for languages that can parse imports).`
+            }
+        }
+    })
+}
+
+const config = require('../../config.json')
+
+module.exports = {
+    label: 'rextester',
+    enabled: true,
+    isSubcommand: false,
+    generator: async (msg, args) => {
+        let language = args[0];
+        let parseLangs = languageProperties.filter(i => i.classDeclaration != undefined).map(j => j.name);
+        let languageObject;
+        let imports = [];
+        if(args[0] == undefined) return outputFullHelp(msg)
+        if(args[0] == 'imports') return outputImports(msg)
+        if(args[0] == 'list') return msg.channel.createMessage("https://hb.wrmsr.io/siraqazugo")
+        let message = await msg.channel.createMessage("Processing, please wait...")
+        languageProperties.forEach(curobject => {
+            if (curobject.aliases.includes(language)) {
+                languageObject = curobject
+            }
+        });
+        if (!languageObject) {
+            languageObject = {
+                name: language,
+                importType: undefined,
+                lineBreak: '',
+                defaultImports: [],
+                aliases: [],
+                classDeclaration: undefined
+            }
+        }
+        if (parseLangs.includes(languageObject.name) && args[1] == languageObject.importType) {
+            parseLanguages(args, imports)
+        };
+        if (args[1] == languageObject.importType && parseLangs.includes(languageObject.name)) {
+            args.splice(0, imports.length + 2)
+        } else {
+            args.splice(0, 1)
+        }
+        let code = args.join(" ")
+        if (languageObject.defaultImports.length > 0 || languageObject.classDeclaration != undefined) {
+            code = parseCode(code, languageObject, imports)
+        }
+        outputResult(msg, language, code, message)
     },
     options: {
-        description: 'Run code via rextester',
-        fullDescription: 'Run code via rextester in a range of languages - do \`..rextester\` for a full help dialogue',
-        usage: '..rextester [help|lang] <import [imports];> [code]',
+        description: 'Use \`..rextester\` for full help',
+        fullDescription: 'Use \`..rextester\` for full help',
+        usage: '..rextester [lang] [imports, if necessary] [code]',
         aliases: ['rex', 'debug'],
     }
 };
