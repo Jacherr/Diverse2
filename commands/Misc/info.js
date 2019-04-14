83341932c752482d97d6e74f7ff6f3cd6066ef29
Jacherr <jwc2002@outlook.com>
2019-04-14 11:09:53 +0100

    better

--- commands/Misc/info.js
+++ commands/Misc/info.js
@@ -1,4 +1,5 @@
 const randomColour = require('../../utils/utils.js').getRandomColor()
+const bot = require('../../bot.js')
 module.exports = {
     label: 'information',
     enabled: true,
@@ -7,7 +8,18 @@ module.exports = {
         msg.channel.createMessage({
             embed: {
                 color: randomColour,
-                description: 'Diverse has been privatised recently so I (Jacher#9891) can focus on making new and better features for it.\nInterested in contributing? DM me (Jacher#9891) or [join the discord](https://discord.gg/upvJaYq)'
+                description: 'Diverse has been privatised recently so I (Jacher#9891) can focus on making new and better features for it.\nInterested in contributing? DM me (Jacher#9891) or [join the server](https://discord.gg/upvJaYq)',
+                author: {
+                    name: 'Diverse',
+                    url: 'https://github.com/Jacherr/Diverse2',
+                    icon_url: bot.user.avatarURL
+                  },
+                fields: [
+                    {
+                        title: 'Contributors',
+                        value: 'Diverse would not have been possible without the help and support from these amazing people:\nCoalSephos#7566\nKhaaZ#0001\nCrylis#9132\noathompsonjones#5337\nAnd everyone who has contributed code or knowledge to the bot or has supported me in any way!'
+                    }
+                ]
             }
         })
     },
