cac5b551342a86b6783a05d9746006a2a46a6236
Jacher <jwc2002@outlook.com>
2019-04-18 19:42:57 +0100

    a

--- commands/Misc/parser.js
+++ commands/Misc/parser.js
@@ -4,12 +4,13 @@ module.exports = {
     isSubcommand: false,
     generator: async (msg, args) => {
         let toParse = args.join(' ')
+        msg.channel.createMessage(`1 ${toParse}`)
         toParse.replace(/{server}/g, msg.channel.guild.name)
-        msg.channel.createMessage(toParse)
+        msg.channel.createMessage(`2 ${toParse}`)
     },
     options: {
         description: 'Parses code - testing command',
-        fullDescription: 'Parses code- testing command',
+        fullDescription: 'Parses code - testing command',
         usage: '..parse [string]',
         aliases: ['p'],
     }
