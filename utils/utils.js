f5d710ab3b0975a77b5dea4b06e45fbefdbe9fea
Jacher <jwc2002@outlook.com>
2019-04-19 21:58:59 +0100

    a

--- utils/utils.js
+++ utils/utils.js
@@ -108,7 +108,7 @@ function getRandomColor() {
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
-    return parseInt(color, 16);
+    return parseInt(color);
   }
   
 module.exports = {
