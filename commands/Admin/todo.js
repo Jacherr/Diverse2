0eda9f9e8176c74eb1e191edae8a010558cdc799
Jacher <jwc2002@outlook.com>
2019-04-19 21:46:25 +0100

    nice.

---
+++ commands/Admin/todo.js
@@ -0,0 +1,26 @@
+const utils = require('../../utils/utils.js')
+module.exports = {
+    label: 'todo',
+    enabled: true,
+    isSubcommand: false,
+    generator: async (msg, args) => {
+        let sendChnnel = bot.guilds.find(i => i.id == '541033778655526912').channels.find(i => i.id == '566259448750800897')
+        sendChnnel.createMessage({
+            embed: {
+                author: {
+                    name: bot.user.username,
+                    icon: bot.user.avatarURL
+                },
+                title: `Todo`,
+                color: utils.getRandomColor(),
+                description: args.join(' ')
+            }
+        })
+    },
+    options: {
+        description: 'Add a todo',
+        fullDescription: 'Add a todo',
+        usage: '..todo [thing]',
+        aliases: ['td'],
+    }
+};
