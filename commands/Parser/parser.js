3d86d22e1c15adbca4d110145e462ada1b6fed97
Jacher <jwc2002@outlook.com>
2019-04-20 19:58:08 +0100

    a

--- commands/Parser/parser.js
+++ commands/Parser/parser.js
@@ -59,6 +59,7 @@ module.exports = {
                 let value = `${individualValues[baseKey][0]}`.substr(0, individualValues[baseKey][0].length - 5)
                 if(toParse.includes(value)) {
                     startPosition = toParse.search(`{${value}:`)
+                    console.log(value)
                     for(let i = startPosition; i < toParse.length; i++) {
                         if(toParse[i] == '}') {
                             endPosition = i
