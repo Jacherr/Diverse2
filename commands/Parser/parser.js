936b944017a2202507b4f7ad9862c03dcefecfeb
Jacher <jwc2002@outlook.com>
2019-04-20 21:00:02 +0100

    a

--- commands/Parser/parser.js
+++ commands/Parser/parser.js
@@ -10,7 +10,7 @@ const getUser = (msg, argument) => {
     if(!founduser) {
         foundUser(msg.member)
     }
-    console.log(founduser.toString().substr(0, 10))
+    console.log(founduser.toString().substr(0, 1000))
 }
 module.exports = {
     label: 'parse',
