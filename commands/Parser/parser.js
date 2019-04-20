98f351a6bf89fc5d02b2331d3689add283d9781c
Jacher <jwc2002@outlook.com>
2019-04-20 13:49:14 +0100

    a

--- commands/Parser/parser.js
+++ commands/Parser/parser.js
@@ -3,6 +3,9 @@ const utils = require('../../utils/utils.js')
 const resetRnd = (msg) => {
     rnduser = msg.channel.guild.members.map(i => i)[Math.floor(Math.random() * msg.channel.guild.members.size)]
 }
+const getUser = (user) => {
+
+}
 module.exports = {
     label: 'parse',
     enabled: true,
@@ -36,20 +39,44 @@ module.exports = {
                 main: '{owner.{property}}',
                 name: ['name', serverOwner.username],
                 discriminator: ['discrim', serverOwner.discriminator],
-                created: ['created', new Date(msg.member.createdAt)]
+                created: ['created', new Date(serverOwner.createdAt).toUTCString()]
             }
         }
         const individualValues = {
-            randomcolor: ['randomcolor', utils.getRandomColor().toString(16)]
+            randomcolor: ['randomcolor', utils.getRandomColor().toString(16)],
+            getuser: ['getuser:{arg}']
         }
         let toParse = args.join(' ')
+        Object.keys(individualValues).forEach(function (baseKey) {
+            if(!individualValues[baseKey][0].includes(':')) {
+                toParse = toParse.replace(new RegExp(`{${individualValues[baseKey][0]}}`, 'g'), individualValues[baseKey][1])
+            } else {
+                let parts = []
+                let stuffToParse
+                let startPosition
+                let endPosition 
+                let argument
+                if(toParse.includes(`{${individualValues[baseKey][0]}:`)) {
+                    startPosition = toParse.search(`{${individualValues[baseKey][0]}:`)
+                    for(let i = startPosition; i < resetRnd.length; i++) {
+                        if(toparse[i] == '}') {
+                            endPosition = i - 1
+                            break
+                        }
+                    }
+                    parts = individualValues[baseKey][0].split(':')
+                    parts[1].replace(new RegExp(`{args}`, 'g'), )
+                    stuffToParse = toparse.substr(startPosition, toParse - endPosition)
+                    argument = stuffToParse.substr(individualValues[baseKey][0].length + 2, stuffToParse.length - 1)
+                    console.log(stuffToParse)
+                    console.log(argument)
+                }
+            }
+        });
         Object.keys(objectValues).forEach(function (baseKey) {
             Object.keys(objectValues[baseKey]).forEach(function (subKey) {
                 toParse = toParse.replace(new RegExp(objectValues[baseKey]['main'].replace('{property}', objectValues[baseKey][subKey][0]), 'g'), objectValues[baseKey][subKey][1])
             });
-        });
-        Object.keys(individualValues).forEach(function (baseKey) {
-            toParse = toParse.replace(new RegExp(`{${individualValues[baseKey][0]}}`, 'g'), individualValues[baseKey][1])
         });
         msg.channel.createMessage(toParse)
     },
