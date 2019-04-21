7a1c46c36cfd15573095c02cc9168c02e6cb03f0
Jacher <jwc2002@outlook.com>
2019-04-21 12:50:20 +0100

    a

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -4,9 +4,26 @@ module.exports = { // export file metadata as an object to base index.js file
     label: 'ping', // name of command
     enabled: true, // if command is enabled
     isSubcommand: false, // if command is a sub command
-    generator: (msg) => { // (client.registerCommand(label, (<stuff>) => {}))
-        let start = Date.now();
-        msg.channel.createMessage('Pong!').then(msg => msg.edit(`Pong! \`${Date.now() - start}ms\``));
+    generator: (msg, args) => { // (client.registerCommand(label, (<stuff>) => {}))
+        let results;
+        let repeat;
+        let flags = require('../../utils/utils.js').resolveFlags(args)
+        if (flags.map(i => i.flagName).includes('c')) repeat = flags.find(i => i.flagName = 'c').flagContent 
+        else repeat = 1
+        let repeat = flags.find(i => i.flagName = 'c').flagContent
+        if (repeat = NaN || repeat > 5 || repeat < 1) {
+            return msg.channel.createMessage('Invalid count! It needs to be between 1 and 5.')
+        }
+        for (let i = 0; i < repeat; i++) {
+            let start = Date.now();
+            let message = await msg.channel.createMessage('Pong!').then(msg => msg.edit(`Pong! \`${Date.now() - start}ms\``));
+            results.push(`${Date.now() - start}`)
+            await message.delete()
+        }
+        let values = results;
+        let sum = values.reduce((previous, current) => current += previous);
+        let avg = sum / values.length;
+        msg.channel.createMessage('Mean ping: \`' + avg + 'ms\`') 
     }, // end command shit
     options: { // options
         description: 'Pings the bot', // to show when a user does "..help"
