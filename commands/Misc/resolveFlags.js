0b3b0ff12143331c1bc5a57aba2304864fbc9e60
Jacher <jwc2002@outlook.com>
2019-04-21 14:51:41 +0100

    a

--- commands/Misc/resolveFlags.js
+++ commands/Misc/resolveFlags.js
@@ -3,7 +3,7 @@ label: 'flag',
 enabled: true,
 isSubcommand: false,
 generator: async (msg, args) => {
-    let splits = args.split("|")
+    let splits = args.join(" ").split("|")
     let result = require('util').inspect(require('../../utils/utils.js').resolveFlags(args[0].split(" "), args[1].split(" ")));
     msg.channel.createMessage(`\`\`\`js\n${result}\`\`\``)
 },
