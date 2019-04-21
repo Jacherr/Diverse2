4407f9d7896279d8d85ab7126ee8bbc74e73c92e
Jacher <jwc2002@outlook.com>
2019-04-21 14:52:17 +0100

    a

--- commands/Misc/resolveFlags.js
+++ commands/Misc/resolveFlags.js
@@ -4,7 +4,7 @@ enabled: true,
 isSubcommand: false,
 generator: async (msg, args) => {
     let splits = args.join(" ").split("|")
-    let result = require('util').inspect(require('../../utils/utils.js').resolveFlags(args[0].split(" "), args[1].split(" ")));
+    let result = require('util').inspect(require('../../utils/utils.js').resolveFlags(splits[0].split(" "), splits[1].split(" ")));
     msg.channel.createMessage(`\`\`\`js\n${result}\`\`\``)
 },
 options: {
