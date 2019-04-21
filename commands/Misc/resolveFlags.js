8e4c4333a6caa8a679b675e01f4bbad77ce1de9b
Jacher <jwc2002@outlook.com>
2019-04-21 14:48:16 +0100

    a

---
+++ commands/Misc/resolveFlags.js
@@ -0,0 +1,16 @@
+module.exports = {
+label: 'flag',
+enabled: true,
+isSubcommand: false,
+generator: async (msg, args) => {
+    let splits = args.split("|")
+    let result = require('util').inspect(require('../../utils/utils.js').resolveFlags(args[0].split(" "), args[1].split(" ")));
+    msg.channel.createMessage(`\`\`\`js\n${result}\`\`\``)
+},
+options: {
+description: 'Resolve flags',
+fullDescription: 'Resolvbe flags',
+usage: '..flag [text to resolve | flags with an argument]',
+aliases: ['f'],
+}
+};
