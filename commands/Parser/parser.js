1268ea825cae81b81aa541e10688d70adef6d4d2
Jacher <jwc2002@outlook.com>
2019-04-23 21:28:39 +0100

    a

--- commands/Parser/parser.js
+++ commands/Parser/parser.js
@@ -25,43 +25,6 @@ module.exports = {
             randomcolor: ['randomcolor', utils.getRandomColor().toString(16)],
             getuser: ['getuser:{arg}']
         }
-        function setObjectValues() {
-            const objectValues = {
-                server: {
-                    main: '{server.{property}}',
-                    name: ['name', msg.channel.guild.name],
-                    members: ['size', msg.channel.guild.members.size],
-                    owner: ['owner', msg.channel.guild.members.get(msg.channel.guild.ownerID).username],
-                    created: ['created', new Date(msg.channel.guild.createdAt).toUTCString()]
-                },
-                user: {
-                    main: '{user.{property}}',
-                    name: ['name', msg.author.username],
-                    joindate: ['joined', new Date(msg.member.joinedAt).toUTCString()],
-                    discriminator: ['discrim', msg.author.discriminator],
-                    created: ['created', new Date(msg.member.createdAt)]
-                },
-                randomuser: {
-                    main: '{randomuser.{property}}',
-                    name: ['name', rnduser.username],
-                    joindate: ['joined', new Date(rnduser.joinedAt).toUTCString()],
-                    discriminator: ['discrim', rnduser.discriminator],
-                    created: ['created', new Date(msg.member.createdAt)]
-                },
-                serverowner: {
-                    main: '{owner.{property}}',
-                    name: ['name', serverOwner.username],
-                    discriminator: ['discrim', serverOwner.discriminator],
-                    created: ['created', new Date(serverOwner.createdAt).toUTCString()]
-                },
-                founduser: {
-                    main: '{founduser.{property}}',
-                    name: ['name', founduser.user.username],
-                    discriminator: ['discrim', founduser.user.discriminator],
-                    created: ['created', new Date(founduser.createdAt).toUTCString()]
-                }
-            }
-        }
         let toParse = args.join(' ')
         Object.keys(individualValues).forEach(function (baseKey) {
             if(!individualValues[baseKey][0].includes(':')) {
@@ -97,7 +60,41 @@ module.exports = {
                 }
             }
         });
-        setObjectValues()
+        const objectValues = {
+            server: {
+                main: '{server.{property}}',
+                name: ['name', msg.channel.guild.name],
+                members: ['size', msg.channel.guild.members.size],
+                owner: ['owner', msg.channel.guild.members.get(msg.channel.guild.ownerID).username],
+                created: ['created', new Date(msg.channel.guild.createdAt).toUTCString()]
+            },
+            user: {
+                main: '{user.{property}}',
+                name: ['name', msg.author.username],
+                joindate: ['joined', new Date(msg.member.joinedAt).toUTCString()],
+                discriminator: ['discrim', msg.author.discriminator],
+                created: ['created', new Date(msg.member.createdAt)]
+            },
+            randomuser: {
+                main: '{randomuser.{property}}',
+                name: ['name', rnduser.username],
+                joindate: ['joined', new Date(rnduser.joinedAt).toUTCString()],
+                discriminator: ['discrim', rnduser.discriminator],
+                created: ['created', new Date(msg.member.createdAt)]
+            },
+            serverowner: {
+                main: '{owner.{property}}',
+                name: ['name', serverOwner.username],
+                discriminator: ['discrim', serverOwner.discriminator],
+                created: ['created', new Date(serverOwner.createdAt).toUTCString()]
+            },
+            founduser: {
+                main: '{founduser.{property}}',
+                name: ['name', founduser.user.username],
+                discriminator: ['discrim', founduser.user.discriminator],
+                created: ['created', new Date(founduser.createdAt).toUTCString()]
+            }
+        }
         Object.keys(objectValues).forEach(function (baseKey) {
             Object.keys(objectValues[baseKey]).forEach(function (subKey) {
                 toParse = toParse.replace(new RegExp(objectValues[baseKey]['main'].replace('{property}', objectValues[baseKey][subKey][0]), 'g'), objectValues[baseKey][subKey][1])
